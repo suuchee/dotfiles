7037f98a345bf65a1c922e5a0802697592ca7d86
hayakawa <86451223+suuchee@users.noreply.github.com>
2022-05-03 19:11:19 +0900

    refactoring

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ function get_symlink_target_dir () {
     local dotfiles_repo=${1}
     local symlink_target_dir=
 
+    if [ ! -d "${dotfiles_repo}" ] ; then
+        printf "no exists \"${dotfiles_repo}\"\n"
+        return 1
+    fi
+
     case "$(uname | tr \"[:upper:]\" \"[:lower:]\")" in
         'linux')
             if [ "$(uname -n)" = 'penguin' ] ; then
@@ -13,7 +18,13 @@ function get_symlink_target_dir () {
 
                 if [ -f '/etc/debian_version' ] ; then
                     symlink_target_dir=${symlink_target_dir}/debian
+                else
+                    printf 'unsupported platform\n'
+                    exit 1
                 fi
+            else
+                printf 'unsupported platform\n'
+                exit 1
             fi
             ;;
         'darwin')
@@ -23,14 +34,18 @@ function get_symlink_target_dir () {
                 # M1 Mac
                 # NOTE: Rosetta is disabled
                 symlink_target_dir=${symlink_target_dir}/m1
+            else
+                # symlink_target_dir=${symlink_target_dir}/intel
+                printf 'unsupported platform\n'
+                exit 1
             fi
 
             # TODO: 
-            printf 'macOS is not supported. But this will be released in the future.\n'
+            printf 'macOS is unsupported. But this will be released in the future.\n'
             exit 0
             ;;
         *)
-            printf 'not supported platform\n'
+            printf 'unsupported platform\n'
             exit 1
     esac
 
@@ -39,61 +54,45 @@ function get_symlink_target_dir () {
 
 if [ ${0} != ${BASH_SOURCE} ] ; then
     printf 'exit\n'
-    return 1
+    return 0
 fi
 
 readonly _MOVE_EXIST_DOTFILES_TO="/tmp/dotfiles_backup/$(date '+%Y%m%d%H%M%S')"
 
-_dotfiles_repo=${1}
-if [ ! "${_dotfiles_repo}" ] ; then
-    _dotfiles_repo=${HOME}/dotfiles
-fi
-
-_symlink_target_dir=$(get_symlink_target_dir ${_dotfiles_repo})
-if [ ! -d ${_symlink_target_dir} ] ; then
-    printf "no exists \"${_symlink_target_dir}\"\n"
-    exit 1
-else
-    :
-fi
-
-# suggest
-_dotfiles=$(find ${_symlink_target_dir} -maxdepth 1 -name ".*" -not -name ".config" -not -name ".ssh")
-printf 'The target files are as follows:\n'
-for df in ${_dotfiles[@]} ; do
-    printf "  $(basename ${df})\n"
-done
-
 # confirm
-printf '\n'
 printf "If a file exists, the file will be moved to \"${_MOVE_EXIST_DOTFILES_TO}\".\n"
 printf 'If a symbolic link exists, the link will be removed.\n'
 read -p 'Would you like to continue? (y/N): ' -n 1 ; printf '\n'
 if [[ ${REPLY} =~ ^[Yy]$ ]] ; then
     mkdir -p ${_MOVE_EXIST_DOTFILES_TO}
+    printf '\n'
 else
     exit 0
 fi
 
 # install
-_exists_df=
-_grep_pattern=
-for df in ${_dotfiles[@]} ; do
-    _exists_df="${HOME}/$(basename ${df})"
-    _grep_pattern="${_grep_pattern} -e ${df}"
+cd "$(get_symlink_target_dir ${HOME}/dotfiles)" || exit 1
 
-    if [ -L ${_exists_df} ] ; then
-        printf "  * unlink ${_exists_df}\n"
-        unlink ${_exists_df}
-    elif [ -f ${_exists_df} ] ; then
-        printf "  * mv ${_exists_df} ${_MOVE_EXIST_DOTFILES_TO}\n"
-        mv ${_exists_df} ${_MOVE_EXIST_DOTFILES_TO}
+for df in $(find "$(pwd)" -maxdepth 1 -name ".*" -not -name ".git" -not -name ".gitignore") ; do
+    exists_df="${HOME}/$(basename ${df})"
+
+    if [ -L "${exists_df}" ] ; then
+        unlink ${exists_df} &&
+        printf "* unlink ${exists_df}\n"
+    elif [ -f "${exists_df}" ] ; then
+        mv ${exists_df} ${_MOVE_EXIST_DOTFILES_TO} &&
+        printf "* mv ${exists_df} ${_MOVE_EXIST_DOTFILES_TO}\n"
+    elif [ -d "${exists_df}" ] ; then
+        # TODO: .config .ssh
+        printf "* ${exists_df} is directory\n"
+        continue
     fi
 
-    ln -s ${_symlink_target_dir}/$(basename ${df}) ${_exists_df}
+    ln -s ${df} ${exists_df} &&
+    printf "  ${exists_df} -> ${df}\n"
 done
 
-printf '\nresults:\n'
-ls -aloX --color='always' ${HOME} | grep ${_grep_pattern}
+# cleanup empty backup directories
+rmdir /tmp/dotfiles_backup/*
 
 exit 0
